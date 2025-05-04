f7376407c66cebaae1cd4fc668abf9dc483875d8
kamilla <kamillasr15@gmail.com>
2025-05-04 01:18:36 -0300

    Treinamento de lógica para captura de dados

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function cadastrarRecruta() {
+  
+    const nome = document.getElementById('nome').value;
+    const sobrenome = document.getElementById('sobrenome').value;
+    const estudo = document.getElementById('estudo').value;
+    const ano = document.getElementById('ano').value;
+    
+
+    console.log(nome, sobrenome, estudo, ano);  
+}
